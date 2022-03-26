1fcffc439622e9a8ebf0174e43b0bf549be2996f
mutohiroshi <mutohiroshi@team-lab.com>
2022-03-26 16:51:38 +0900

    feat: 一覧のカードにリンクを付与

--- pages/questions/received.tsx
+++ pages/questions/received.tsx
@@ -12,6 +12,7 @@ import {
   startAfter,
   where,
 } from 'firebase/firestore'
+import Link from 'next/link'
 import { useAuthentication } from '../../hooks/authentication'
 import { Question } from '../../models/Question'
 import Layout from '../../components/Layout'
@@ -109,14 +110,18 @@ export default function QuestionsReceived() {
       <div className="row justify-content-center">
       <div className="col-12 col-md-6" ref={scrollContainerRef}>
           {questions.map((question: Question) => (
-            <div className="card my-3" key={question.id}>
-              <div className="card-body">
-                <div className="text-truncate">{question.body}</div>
-                <div className="text-muted text-end">
-                  <small>{dayjs(question.createdAt.toDate()).format('YYYY/MM/DD HH:mm')}</small>
+            <Link href={`/questions/${question.id}`} key={question.id}>
+              <a>
+                <div className="card my-3">
+                  <div className="card-body">
+                    <div className="text-truncate">{question.body}</div>
+                    <div className="text-muted text-end">
+                      <small>{dayjs(question.createdAt.toDate()).format('YYYY/MM/DD HH:mm')}</small>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              </a>
+            </Link>
           ))}
         </div>
       </div>
